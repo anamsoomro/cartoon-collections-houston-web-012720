adce1e6eca94ec58b71e34fa99992c0901d824c3
anamsoomro <anamsoomroed@gmail.com>
2020-01-27 15:08:34 -0600

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)
+  dwarfs.each_with_index do |item, index|
+    puts "#{index+1} #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |word|
+    word.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  # if snacks.include? (cheese_types)
+    snacks.find {|snack| cheese_types.include? (snack)}
+  # else 
+  #   nil
+  # end
+
+  # I dont need that if statement, because if find finds no elements that satisifies, it will itself return nil
 end
